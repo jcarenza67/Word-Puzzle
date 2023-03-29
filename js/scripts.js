21d233e6b0f4d91b9c9754d5fc2ab83e75cde4b0
Joseph Wilfong <josephwilfong91@gmail.com>
2023-03-29 13:29:39 -0700

    add button, type bar, and results

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
 window.addEventListener("load", function(){
-  this.event.preventDefault()
+  const submitBtn = document.getElementById("submit")
 
-  const 
-})
+  submitBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const inputString = document.getElementById("input").value;
+    const result = wordPuzzle(inputString);
+
+    const resultDiv = document.getElementById("puzzle")
+    resultDiv.textContent = result;
+  });
+});   
+    function wordPuzzle(string) {
+      const vowels = ['a', 'e', 'i', 'o', 'u'];
+      let puzzle = '';
+    
+      for (let i = 0; i < string.length; i++) {
+        const letter = string[i];
+        if (vowels.includes(letter.toLowerCase())) {
+          puzzle += '-';
+        } else {
+          puzzle += letter;
+        }
+      }
+    
+      return puzzle;
+    }
