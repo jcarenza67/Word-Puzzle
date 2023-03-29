e3ce88dae685f01fc0d6820cbf185ecc517d7d9d
Joseph Wilfong <josephwilfong91@gmail.com>
2023-03-29 14:31:04 -0700

    update code for best practices

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,30 @@
-window.addEventListener("load", function(){
-  const submitBtn = document.getElementById("submit")
+function wordPuzzle(string) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let puzzle = '';
+  
+  for (let i = 0; i < string.length; i++) {
+    const letter = string[i];
+    if (vowels.includes(letter.toLowerCase())) {
+      puzzle += '-';
+    } else {
+      puzzle += letter;
+    }
+  }
+  
+  return puzzle;
+}
+
+window.addEventListener("load", function() {
+  const submitBtn = document.getElementById("submit");
+  const inputField = document.getElementById("input");
+  const resultDiv = document.getElementById("puzzle");
 
   submitBtn.addEventListener("click", function(event) {
     event.preventDefault();
 
-    const inputString = document.getElementById("input").value;
+    const inputString = inputField.value;
     const result = wordPuzzle(inputString);
 
-    const resultDiv = document.getElementById("puzzle")
     resultDiv.textContent = result;
   });
-});   
-    function wordPuzzle(string) {
-      const vowels = ['a', 'e', 'i', 'o', 'u'];
-      let puzzle = '';
-    
-      for (let i = 0; i < string.length; i++) {
-        const letter = string[i];
-        if (vowels.includes(letter.toLowerCase())) {
-          puzzle += '-';
-        } else {
-          puzzle += letter;
-        }
-      }
-    
-      return puzzle;
-    }
+});
